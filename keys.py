dcf7e34eed5d4bdcd8c31aa5978b1690a473361e
Ellie Le <69995236+elliele@users.noreply.github.com>
2021-05-17 20:29:25 -0700

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,10 @@
+# Created by Ellie Le at 5/12/2021
+
+# Enter personal keys from twitter deverloper account.
+
+keys = dict(
+api_key = 'xxxxxxx',
+api_secret_key = 'xxxxxxx',
+access_token  = 'xxxxxxxx',
+access_token_secret  = 'xxxxxxxxxxxxxxx',
+)
